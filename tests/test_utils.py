7a79ae7e4dcd89f7f3b96731138c34d2e7892edd
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 18:51:00 +0300

    done tests for utils (method TDD)

---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import json
+from unittest.mock import patch
+
+from freezegun import freeze_time
+from pandas import DataFrame, Timestamp
+
+from config import PATH_TO_OPERATIONS, PATH_TO_USER_SETTINGS
+from src.greeting import greeting
+
+
+# from src.utils import
+
+def test_greeting():
+    with freeze_time("2025-01-23 00:00:00"):
+        assert greeting() == "Доброй ночи"
+
+    with freeze_time("2025-01-23 08:00:00"):
+        assert greeting() == "Доброе утро"
+
+    with freeze_time("2025-01-23 12:00:00"):
+        assert greeting() == "Добрый день"
+
+    with freeze_time("2025-01-23 18:00:00"):
+        assert greeting() == "Добрый вечер"
+
+
