d3124a45810b8aed3aecfa3f8265f5a7cfb1efc7
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 21:54:42 +0300

    added function for cashback_categories

--- src/pd.py
+++ src/pd.py
@@ -1,12 +1,15 @@
 import json
-
 import pandas as pd
 from config import PATH_TO_OPERATIONS
+import json
+import openpyxl
 
-df = pd.read_excel(PATH_TO_OPERATIONS)
-df = df[df["Сумма операции"] < 0]
+def cashback_categories():
+    """Функция возврата суммы всех операций по категориям в json-формате для последующего анализа выгодности кешбека"""
+    df = pd.read_excel(PATH_TO_OPERATIONS)
+    df = df[df["Сумма операции"] < 0]
 
-total_df = df[["Категория", "Сумма операции"]].groupby("Категория").sum().reset_index()
+    total_df = df[["Категория", "Сумма операции"]].groupby("Категория").sum().reset_index()
 
-data = total_df.to_dict("records")
-print(json.dumps(data, ensure_ascii=False, indent=4))
+    data = total_df.to_dict("records")
+    return json.dumps(data, ensure_ascii=False, indent=4)
