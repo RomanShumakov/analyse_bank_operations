ec315e62dab68ce163db5d4ede9b8f115f764284
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 20:07:13 +0300

    complete function greeting (time) and tests

--- src/greeting.py
+++ src/greeting.py
@@ -6,7 +6,7 @@ def greeting():
     if 6 <= enter_time < 12:
         return "Доброе утро"
     elif 12 <= enter_time < 18:
-        return "Доброе день"
+        return "Добрый день"
     elif 18 <= enter_time < 24:
         return "Добрый вечер"
     elif 0 <= enter_time < 6:
