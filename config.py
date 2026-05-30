743b93be842cc0665d954655ea1f19f6d296f213
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 18:34:59 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PATH = Path(__file__).parent
+PATH_TO_OPERATIONS = Path / "data" / "operations.xlsx"
+PATH_TO_USER_SETTINGS = Path / "user_settings.json"
+PATH_TO_LOGGER = Path / "logs"
