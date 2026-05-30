0b5d931cd873ffad05f6c3b2c884cab3e071715b
Роман Шумаков <romeo46100@mail.ru>
2026-05-30 19:03:53 +0300

    changed pd draft

--- src/pd.py
+++ src/pd.py
@@ -1,7 +1,12 @@
+import json
+
 import pandas as pd
 from config import PATH_TO_OPERATIONS
 
 df = pd.read_excel(PATH_TO_OPERATIONS)
-df = df[df[Эсумам] < 0]
+df = df[df["Сумма операции"] < 0]
 
-total_df = def[["Категория", "Сумма операции"]].groupby("Категория").sum().reset_index()
+total_df = df[["Категория", "Сумма операции"]].groupby("Категория").sum().reset_index()
+
+data = total_df.to_dict("records")
+print(json.dumps(data, ensure_ascii=False, indent=4))
