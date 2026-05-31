bef5303ec0d603e427a46ff7b67eca1c10176de6
Роман Шумаков <romeo46100@mail.ru>
2026-05-31 16:24:28 +0300

    done get_currency_stocks for russian market MOEX

--- src/api_request.py
+++ src/api_request.py
@@ -1,20 +1,21 @@
 import requests
 import json
 from config import PATH_TO_USER_SETTINGS
+from typing import Generator, Dict
 
 
-def settings_reader() -> list:
-    """Расшифровка пользовательских конфигураций с извлечением валютных кодов"""
+def settings_reader() -> dict[list]:
+    """Расшифровка пользовательских конфигураций"""
     with open(PATH_TO_USER_SETTINGS, "r", encoding='utf-8') as file:
-        settings_dict = json.load(file)
-        return settings_dict["user_currencies"]
+        return json.load(file)
 
-# res = settings_reader()
-# print(res)
+settings_dict = settings_reader()
+coder = settings_dict["user_currencies"]
+stocker = settings_dict["user_stocks"]
 
 
-def get_currency_rate(codes: list) -> dict:
-    """Получение настроенных пользователем курсов валют от ЦБ РФ"""
+def get_currency_rate(codes: list) -> Generator[dict]:
+    """Функция получения настроенных пользователем курсов валют от ЦБ РФ"""
     jsonfile = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
     datafile = jsonfile.json()
     for valute_code in datafile["Valute"]:
@@ -24,8 +25,10 @@ def get_currency_rate(codes: list) -> dict:
                 "rate": datafile["Valute"][valute_code]["Value"]
             }
 
-# codes = ["USD", "EUR"]
-# rate = get_currency_rate(codes)
-# print(list(rate))
 
-api_2 = "https://api.frankfurter.dev/v2/rates?base=USD&quotes=RUB"
+def get_currency_stocks(stocks: list) -> Generator[dict]:
+    """Функция получения цен настроенных пользователем акций по интернет-запросу к сайту iss.moex.com"""
+    for stock in stocks:
+        jsonfile = requests.get(f"https://iss.moex.com/iss/engines/stock/markets/shares/securities/{stock}.json")
+        datafile = jsonfile.json()
+        yield {"stock": stock, "price": datafile["securities"]["data"][0][3]}
