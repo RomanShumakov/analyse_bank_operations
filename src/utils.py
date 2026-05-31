ebbc05e7157e03a3663c321160dfc4b095b25fd0
Роман Шумаков <romeo46100@mail.ru>
2026-05-31 17:53:26 +0300

    replace greeting from greeting.py to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,17 @@
 import json
 from datetime import datetime
 
+def greeting():
+    enter_time = datetime.now().hour
+    if 6 <= enter_time < 12:
+        return "Доброе утро"
+    elif 12 <= enter_time < 18:
+        return "Добрый день"
+    elif 18 <= enter_time < 24:
+        return "Добрый вечер"
+    elif 0 <= enter_time < 6:
+        return "Доброй ночи"
+
 import pandas as pd
 from config import PATH_TO_OPERATIONS, PATH_TO_USER_SETTINGS
 import openpyxl
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,24 +5,7 @@ from freezegun import freeze_time
 # from pandas import DataFrame, Timestamp
 #
 # from config import PATH_TO_OPERATIONS, PATH_TO_USER_SETTINGS
-from src.greeting import greeting
-
-
-# from src.utils import
-#
-# def test_greeting():
-#     with freeze_time("2025-01-23 00:00:00"):
-#         assert greeting() == "Доброй ночи"
-#
-#     with freeze_time("2025-01-23 08:00:00"):
-#         assert greeting() == "Доброе утро"
-#
-#     with freeze_time("2025-01-23 12:00:00"):
-#         assert greeting() == "Добрый день"
-#
-#     with freeze_time("2025-01-23 18:00:00"):
-#         assert greeting() == "Добрый вечер"
-
+from src.utils import greeting
 
 
 def test_greeting():
