c3fdf6a3c8c4f95baac8763063df1de387ca715e
Роман Шумаков <romeo46100@mail.ru>
2026-05-31 11:56:48 +0300

    changed services category sum

--- src/services.py
+++ src/services.py
@@ -8,9 +8,12 @@ import openpyxl
 def cashback_categories():
     """Функция возврата суммы всех операций по категориям в json-формате для последующего анализа выгодности кешбека"""
     df = pd.read_excel(PATH_TO_OPERATIONS)
-    df = df[df["Сумма операции"] < 0]
+    df = df[df["Сумма операции"] > 0]
 
     total_df = df[["Категория", "Сумма операции"]].groupby("Категория").sum().reset_index()
 
     data = total_df.to_dict("records")
     return json.dumps(data, ensure_ascii=False, indent=4)
+
+a = cashback_categories()
+print(a)
